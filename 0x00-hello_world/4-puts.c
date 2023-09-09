0ba11f6b4c525085eaef39710dbe84562bc3ba38
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-09 19:00:01 +0300

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - printig with puts
- *
- * Return: 0
- */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+    printf("Testing Betty\n");
+    return (0);
+}
