ab46da4ad46e03ff10f361538a86b13fea29451d
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-12 23:09:36 +0300

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,29 +1,13 @@
 #include "main.h"
 
-void print_alphabet(void);
-
 /**
-  * main - calls print_alphabet function
-  *Return: 0
-  */
-
-int main(void)
-{
-	print_alphabet();
-	return (0);
-}
-
-/**
- * print_alphabet - prints the alphabet in lowercase
+ * print_alphabet - prints alphabets in lower case
  */
-
 void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
-	{
 		putchar(c);
-		putchar('\n');
-	}
+	putchar('\n');
 }
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -1,18 +1,12 @@
-#ifndef __MAIN_H__
-#define __MAIN_H__
+#include "main.h"
 
-int _putchar(int c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
-
-#endif
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_alphabet();
+    return (0);
+}
