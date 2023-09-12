e4b63ce212c7025a2b4cdcf4f6f0d1848c21d551
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-12 23:39:28 +0300

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_alphabet - print all alphabet in lowercase
+ */
+
+void print_alphabet(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		_putchar(x);
+
+	_putchar('\n');
+}
