04e9111582760e48319eeb7c15d1ff5714113389
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-14 17:58:19 +0300

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -8,7 +8,7 @@
 int main(void)
 {
 	int i;
-	
+
 	printf("Infinite loop incoming :(\n");
 	/**
 	*i = 0;
@@ -19,6 +19,6 @@ int main(void)
 	*}
 	*/
 	printf("Infinite loop avoided! \\o/\n");
-	
+
 	return (0);
 }
