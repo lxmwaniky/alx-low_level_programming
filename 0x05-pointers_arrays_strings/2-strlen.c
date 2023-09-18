4abde26bb6e2d67fb24f6dbb2c8a31361a8a78cc
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-18 18:42:54 +0300

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+  *_strlen - gets length of string
+  *@s: Value
+  *Return: Length of string
+  */
+int _strlen(char *s)
+{
+	return (strlen(s));
+}
