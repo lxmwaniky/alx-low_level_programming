9d2ca936cca45572f467290c757a53a6d47299b2
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-18 21:34:20 +0300

    6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * puts2 - prints one char out of 2 of a string
+ * followed by a new line
+ * @str: string to print the chars from
+ */
+void puts2(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j += 2)
+	{
+		_putchar(str[j]);
+	}
+	_putchar('\n');
+}
