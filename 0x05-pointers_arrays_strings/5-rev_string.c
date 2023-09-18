d00e4ca0e0806a6cf01c6ead0e7ddeff16284f4d
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-18 21:35:53 +0300

    5-rev_string.c

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -6,20 +6,20 @@
  */
 void rev_string(char *s)
 {
-	char tmp;
-	int i, len, len1;
+	char store;
+	int i, j, k;
 
-	len = 0;
-	len1 = 0;
-	while (s[len] != '\0')
+	j = 0;
+	k = 0;
+	while (s[j] != '\0')
 	{
-		len++;
+		k++;
 	}
-	len1 = len - 1;
-	for (i = 0; i < len / 2; i++)
+	k = j - 1;
+	for (i = 0; i < j / 2; i++)
 	{
-		tmp = s[i];
-		s[i] = s[len1];
-		s[len1--] = tmp;
+		store = s[i];
+		s[i] = s[k];
+		s[k--] = store;
 	}
 }
