916e01c78d883dd0c872309b53699790b0130193
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-20 12:24:37 +0300

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,9 @@
 #include "main.h"
 /**
  * _strcat - concatenates two strings
- * @dest: string to append to
- * @src: string to add
- *
- * Return: a pointer to the resulting string
+ * @dest: destination string
+ * @src: source string
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
