1fcf00e98f94893112c4456e525d7248884dad87
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-21 16:10:57 +0300

    7-leet.c

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * leet - encodes a string
+ * @s: string being encoded
+ * Return: the encoded string;
+ */
+char *leet(char *s)
+{
+	int i, j;
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+	return (s);
+}
