a73724b6ba58ebc243f154adab7fc1a0aa6172f0
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-21 16:15:08 +0300

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_number - prints a no.
+ * @n: no. being printed;
+ */
+void print_number(int n)
+{
+	unsigned int n1;
+
+	if (n < 0)
+	{
+		n1 = -n;
+		_putchar('-');
+	}
+	else
+	{
+		n1 = n;
+	}
+	if (n1 / 10)
+		print_number(n1 / 10);
+		_putchar((n1 % 10) + '0');
+}
