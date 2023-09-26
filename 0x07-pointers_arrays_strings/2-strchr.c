4668228056803a45804253e16c099deac82a077c
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-26 10:32:49 +0300

    2-strchr.c

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -10,16 +10,16 @@
  */
 char *_strchr(char *s, char c)
 {
-	int a;
+	int n;
 
 	while (1)
 	{
-		a = *s++;
-		if (a == c)
+		n = *s++;
+		if (n == c)
 		{
 			return (s - 1);
 		}
-		if (a == 0)
+		if (n == 0)
 		{
 			return (NULL);
 		}
