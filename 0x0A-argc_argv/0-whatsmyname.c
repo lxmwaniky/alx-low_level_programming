f8c5473aef15b94adac7cf63ac6df1dafb50787c
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-09-30 01:21:11 +0300

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * desc: prints a program name using arguments
+ * @argc: parameter argc
+ * @argv: parameter argv
+ * Return: 0 if works
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
