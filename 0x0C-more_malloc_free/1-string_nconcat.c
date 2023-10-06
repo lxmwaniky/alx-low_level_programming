c2b80ac4f0d79f0b9e319c544f5a266ef589b690
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-10-06 05:57:04 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,26 +11,26 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int len1 = 0, len2 = 0, x, y;  
-    char *new_string;
+	unsigned int len1 = 0, len2 = 0, x, y;
+	char *new_string;
 
-    if (s1 == NULL)
-        s1 = "";
-    if (s2 == NULL)
-        s2 = "";
-    while (s1[len1])
-        len1++;
-    while (s2[len2])
-        len2++;
-    if (n >= len2)
-        n = len2;
-    new_string = malloc(len1 + n + 1);
-    if (new_string == NULL)
-        return (NULL);
-    for (x = 0; x < len1; x++)
-        new_string[x] = s1[x];
-    for (y = 0; y < n; y++, x++)
-        new_string[x] = s2[y];
-    new_string[x] = '\0';   /*proper termination*/
-    return (new_string);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[len1])
+		len1++;
+	while (s2[len2])
+		len2++;
+	if (n >= len2)
+		n = len2;
+	new_string = malloc(len1 + n + 1);
+	if (new_string == NULL)
+		return (NULL);
+	for (x = 0; x < len1; x++)
+		new_string[x] = s1[x];
+	for (y = 0; y < n; y++, x++)
+		new_string[x] = s2[y];
+	new_string[x] = '\0';   /*proper termination*/
+	return (new_string);
 }
