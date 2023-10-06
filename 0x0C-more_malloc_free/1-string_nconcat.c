c4e7acfde5b747342aa83741ce6716a256589834
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-10-06 22:04:46 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,62 +1,33 @@
 #include "main.h"
+#include <stdlib.h>
 
-/* helper functions */
-#define empty_if_null(s) ((s == NULL) ? (s = "") : s)
-unsigned int _strlen(const char *s);
-
-/**
- * string_nconcat - concatenate two strings
- * @s1: string 1
- * @s2: string 2
- * @n: number of characters to write from string @s2 to
- * the new string
- *
- * Note: Memory for the new string is dynamically allocated,
- * and should be freed.
- *
- * Return: A pointer to the concatenated string
- */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	char *new_str;
-	unsigned int s1_len, s2_len, new_str_len, i, j;
+    unsigned int len1, len2, i, j;
+    char *result;
 
-	/* handle as an empty string if NULL */
-	empty_if_null(s1);
-	empty_if_null(s2);
+    if (s1 == NULL)
+        s1 = "";
 
-	s1_len = _strlen(s1);
-	s2_len = _strlen(s2);
+    for (len1 = 0; s1[len1]; len1++)
+        ;
+    for (len2 = 0; s2[len2]; len2++)
+        ;
 
-	if (n >= s2_len)
-		n = s2_len; /* get the exact number of characters to write from s2 */
+    if (n >= len2)
+        n = len2;
+    result = malloc(len1 + n + 1);
 
-	new_str_len = s1_len + n;
-	new_str = malloc(sizeof(char) * (new_str_len + 1));
+    if (result == NULL)
+        return NULL;
 
-	if (new_str == NULL)
-		return (NULL); /* handle memory allocation failure */
+    for (i = 0; i < len1; i++)
+        result[i] = s1[i];
 
-	/* Concatenate s1 and s2 to new_str */
-	for (i = 0; i < s1_len; i++)
-		new_str[i] = s1[i];
-	for (j = 0; j < n; j++)
-		new_str[i + j] = s2[j];
-	new_str[i + j] = '\0';
+    for (j = 0; j < n; j++)
+        result[i + j] = s2[j];
 
-	return (new_str);
-}
+    result[i + j] = '\0';
 
-/**
- * _strlen - get the length of a string
- * @s: string
- *
- * Return: the length of string @s
- */
-unsigned int _strlen(const char *s)
-{
-	unsigned int len;
-
-	for (len = 0; s[len] != '\0'; len++);
-	return (len);
+    return result;
 }
