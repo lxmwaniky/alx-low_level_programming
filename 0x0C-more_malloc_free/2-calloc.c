a6da13cd8ed55bf8625371b3a4b7df0eaaeb147f
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-10-06 05:43:28 +0300

    2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+void _memset(char *s, int c, unsigned int n);
+/**
+ * _calloc - allocate memory
+ * @nmemb: no of elements
+ * @size: no of bytes
+ * Return: pointer, NULL when unsuccessful
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *point;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	point = malloc(nmemb * size);
+	if (point == NULL)
+		return (NULL);
+
+	_memset(point, 0, nmemb * size);
+
+	return (point);
+}
+
+/**
+ * _memset - fill memory
+ * @s: string buffer
+ * @c: byte
+ * @n: no of bytes
+ */
+void _memset(char *s, int c, unsigned int n)
+{
+	unsigned int x;
+
+	for (x = 0; x < n; x++)
+		s[x] = c;
+}
