54a7b86a2fced7785cb9d9b52e6072843d8abf2b
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-10-06 21:53:39 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,36 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "main.h"
 
+/* helper functions */
+#define empty_if_null(s) ((s == NULL) ? (s = "") : s)
+unsigned int _strlen(const char *s);
+
 /**
- * string_nconcat - Concatenates two strings
- * @s1: first string.
- * @s2: second string.
- * @n: The max no of bytes
- * Return: A pointer
+ * string_nconcat - concatenate two strings
+ * @s1: string 1
+ * @s2: string 2
+ * @n: number of characters to write from string @s2 to
+ * the new string
+ *
+ * Note: Memory for the new string is dynamically allocated,
+ * and should be freed.
+ *
+ * Return: A pointer to the concatenated string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int len1 = 0, len2 = 0, x, y;
-	char *new_string;
+	char *new_str;
+	unsigned int s1_len, s2_len, new_str_len, i, j;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[len1])
-		len1++;
-	while (s2[len2])
-		len2++;
-	if (n >= len2)
-		n = len2;
-	new_string = malloc(len1 + n + 1);
-	if (new_string == NULL)
-		return (NULL);
-	for (x = 0; x < len1; x++)
-		new_string[x] = s1[x];
-	for (y = 0; y < n; y++, x++)
-		new_string[x] = s2[y];
-	new_string[x] = '\0';   /*proper termination*/
-	return (new_string);
+	/* handle as an empty string if NULL */
+	empty_if_null(s1);
+	empty_if_null(s2);
+
+	s1_len = _strlen(s1);
+	s2_len = _strlen(s2);
+
+	if (n >= s2_len)
+		n = s2_len; /* get the exact number of characters to write from s2 */
+
+	new_str_len = s1_len + n;
+	new_str = malloc(sizeof(char) * (new_str_len + 1));
+
+	if (new_str == NULL)
+		return (NULL); /* handle memory allocation failure */
+
+	/* Concatenate s1 and s2 to new_str */
+	for (i = 0; i < s1_len; i++)
+		new_str[i] = s1[i];
+	for (j = 0; j < n; j++)
+		new_str[i + j] = s2[j];
+	new_str[i + j] = '\0';
+
+	return (new_str);
+}
+
+/**
+ * _strlen - get the length of a string
+ * @s: string
+ *
+ * Return: the length of string @s
+ */
+unsigned int _strlen(const char *s)
+{
+	unsigned int len;
+
+	for (len = 0; s[len] != '\0'; len++);
+	return (len);
 }
