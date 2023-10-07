89137430dfc906b9419f4ab21a5a1763f87832b6
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-10-07 05:54:59 +0300

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -3,16 +3,14 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-    unsigned int len1, len2, i, j;
+    unsigned int len1, len2, x, y;
     char *result;
 
     if (s1 == NULL)
         s1 = "";
 
-    for (len1 = 0; s1[len1]; len1++)
-        ;
-    for (len2 = 0; s2[len2]; len2++)
-        ;
+    for (len1 = 0; s1[len1]; len1++);
+    for (len2 = 0; s2[len2]; len2++);
 
     if (n >= len2)
         n = len2;
@@ -21,13 +19,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
     if (result == NULL)
         return NULL;
 
-    for (i = 0; i < len1; i++)
-        result[i] = s1[i];
+    for (x = 0; x < len1; x++)
+        result[x] = s1[x];
 
-    for (j = 0; j < n; j++)
-        result[i + j] = s2[j];
+    for (y = 0; y < n; y++)
+        result[x + y] = s2[y];
 
-    result[i + j] = '\0';
+    result[x + y] = '\0';
 
     return result;
 }
