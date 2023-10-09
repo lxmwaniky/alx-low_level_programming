813091d9d6e3db129d7134e2a107ede7e4f9cbc9
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-10-10 01:48:58 +0300

    Dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,15 +1,12 @@
 #ifndef DOG_H
 #define DOG_H
-/**
-  *struct dog - Dog details
-  *@name: Dog's name
-  *@age: Dog's age
-  *@owner: Dog's owner
-  */
 struct dog
 {
 	char *name;
-	float *age;
+	float age;
 	char *owner;
-}
+};
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+
 #endif
