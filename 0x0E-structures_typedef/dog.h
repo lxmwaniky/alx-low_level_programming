0d9e66b347b29f6a9a72002b3aa49216a219d0d0
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-10-10 01:32:01 +0300

    0x0E-structures

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - Dog details
+  *@name: Dog's name
+  *@age: Dog's age
+  *@owner: Dog's owner
+  */
+struct dog
+{
+	char *name;
+	float *age;
+	char *owner;
+}
+#endif
