ae8000af861faefc43d2206169221d8499c20df6
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-10-11 22:32:57 +0300

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * array_iterator - iterates func ptr
+ * @array: array
+ * @size: size of array
+ * @action: functon pointer
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int x;
+
+	if (array != NULL && action != NULL)
+	{
+		for (x = 0; x < size; x++)
+			action(array[x]);
+	}
+}
