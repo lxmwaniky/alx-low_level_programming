010c471bb927cba9b67c4e077c9a7ff946661bad
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-10-11 22:52:11 +0300

    3-op_functions.c

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,32 @@
+#include <string.h>
+#include "3-calc.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - gets math operator
+ * @s: operator passed
+ * Return: integer
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].op != NULL)
+	{
+		if (strcmp(s, ops[i].op) == 0)
+			return (ops[i].f);
+		i++;
+	}
+
+	return (NULL);
+}
