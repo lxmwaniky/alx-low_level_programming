5d7c53abd71be198be591956e5c4e5cd2a057cc8
Alex Mwaniki Nyambura <lekko254@gmail.com>
2023-12-21 20:02:43 +0300

    5-hash_table_print.c

---
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_print - function to print all elements of a hash table.
+ * @ht: The hash table to be printed.
+*/
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i = 0;
+	hash_node_t *current;
+	int first = 1;
+
+	if (ht == NULL)
+		return;
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		current = ht->array[i];
+		while (current != NULL)
+		{
+			if (first)
+				first = 0;
+			else
+				printf(", ");
+			printf("'%s': '%s'", current->key, current->value);
+			current = current->next;
+		}
+	}
+	printf("}\n");
+}
